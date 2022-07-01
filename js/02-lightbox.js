707d515950b0556c06a02dd9691cabba6e292da0
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-01 17:56:32 +0300

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,50 +3,36 @@ import { galleryItems } from './gallery-items.js';
 const galleryListRef = document.querySelector('.gallery');
 
 const itemsList = galleryItems
-    .map(({ preview, original, description }) => {
-        const divEl = document.createElement('div');
-        divEl.classList.add('gallery__item');
+    .map(({ original, preview, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`
+    })
+    .join("");
+galleryListRef.insertAdjacentHTML("beforeend", itemsList);
 
-        const linkEl = document.createElement('a');
-        linkEl.classList.add("gallery__link")
-        linkEl.href = "#";
-
-        const imgEl = document.createElement('img');
-        imgEl.classList.add("gallery__image");
-        imgEl.src = preview;
-        imgEl.dataset.action  = `large-image.jpg`;
-    imgEl.alt = description;
-
-  linkEl.append(imgEl);
-  divEl.append(linkEl);
-  return divEl;
-});
 console.log(itemsList);
-galleryListRef.append(...itemsList);
 console.log(galleryItems);
 // <----------Modal------->
-let modalWindow;
-const openModal = (e) => {
-    e.preventDefault();
-    if (e.target.nodeName !== "IMG") {
-    return;
-    }
-    modalWindow = basicLightbox.create(`
-    <img src="${e.target.src}" width="800" height="600">
-`, {
-        onShow: (openModal) => { document.addEventListener('keydown', onEscPress) }, 
-        onClose: (openModal) => { document.removeEventListener('keydown', onEscPress) }
-    });
-    console.log('open window');;
-    modalWindow.show();    
-}
-galleryListRef.addEventListener('click', openModal);
+let modalWindow = new SimpleLightbox(
+        '.gallery a', { captionsData: 'alt', captionDelay: 250 }
+);
+  
 
-const onEscPress = event => {
-  console.log(event.key);
-  if (event.key === 'Escape') {
-      console.log('Escape close window');
-      modalWindow.close();
-      
-  }
-};
+// <-----варіант 2 з append----->
+// const itemsList = galleryItems
+//     .map(({ preview, original, description }) => {
+//         const divEl = document.createElement('div');
+//         const linkEl = document.createElement('a');
+//         linkEl.classList.add("gallery__item")
+//         linkEl.href = original;
+//         const imgEl = document.createElement('img');
+//         imgEl.classList.add("gallery__image");
+//         imgEl.src = preview;
+//         imgEl.alt = description;
+
+//   linkEl.append(imgEl);
+//   divEl.append(linkEl);
+//   return divEl;
+// });
+// galleryListRef.append(...itemsList);
